46a2a2179c4bf8af58c7940af40271ca37a361ca
zenblatchley <79449335+zenblatchley@users.noreply.github.com>
2024-07-07 10:13:41 -0700

    Add files via upload

---
+++ web-projects/notes/js pop up notes.js
@@ -0,0 +1,14 @@
+let myVariable = document.querySelector("h1");
+alert("hello!");
+document.querySelector("html").addEventListener("click", function () {
+    alert("Ouch! Stop poking me!");
+  });
+  
+// hello = auto pop up when age loads
+// ouch! = pop up when you click on something on the page
+
+/*
+Everything in between is a comment.
+*/
+
+// This is a comment
